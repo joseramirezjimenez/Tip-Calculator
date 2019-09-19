d649a159687abfc67117a604f9a1313b4a1c760e
joseramirezjimenez <jose.ramirez5019b@gmail.com>
2019-09-19 10:56:55 -0500

    Added drop down menu, added another text input Added a button and added more style to CSS file

---
+++ JS/scripts.js
@@ -0,0 +1,5 @@
+//Function that is executed when the button is clicked
+//We are passing an anoymous function (a function without a name to the button's onclick handler when it's clikced)
+document.getElementById("calculateTip").onclick = function() {
+    console.log("Button was clicked");
+}
